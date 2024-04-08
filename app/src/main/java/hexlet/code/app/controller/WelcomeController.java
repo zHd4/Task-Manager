acf38b23ca8acaf8e6c5219d66c1cb30c5f4c8eb
zHd4 <computersystembase@gmail.com>
2024-04-08 16:02:03 +0200

    Add WelcomeController

---
+++ app/src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package hexlet.code.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/welcome")
+public class WelcomeController {
+    @GetMapping
+    public String index() {
+        return "Welcome to Spring";
+    }
+}
