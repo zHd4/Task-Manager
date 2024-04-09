3e5782f8ebf698b67a514dc1855321e722593e21
zHd4 <computersystembase@gmail.com>
2024-04-09 17:42:55 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-configuration-processor")
+	implementation("org.postgresql:postgresql")
 //	implementation("org.springframework.boot:spring-boot-starter-security")
 //	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
