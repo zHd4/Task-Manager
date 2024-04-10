936c378ad11997de4dff56c98723feef23a5ad83
zHd4 <computersystembase@gmail.com>
2024-04-11 01:46:06 +0200

    Rename UserDetailsService.java -> CustomUserDetailsService

--- app/src/main/java/hexlet/code/app/service/CustomUserDetailsService.java
+++ app/src/main/java/hexlet/code/app/service/CustomUserDetailsService.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.provisioning.UserDetailsManager;
 
-public class UserDetailsService implements UserDetailsManager {
+public class CustomUserDetailsService implements UserDetailsManager {
     @Autowired
     private UserRepository userRepository;
 
