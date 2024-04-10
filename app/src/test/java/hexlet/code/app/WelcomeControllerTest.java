5af81451ade76600ee361de2ea0430fdb16dcf93
zHd4 <computersystembase@gmail.com>
2024-04-11 00:28:16 +0200

    Create WelcomeControllerTest

---
+++ app/src/test/java/hexlet/code/app/WelcomeControllerTest.java
@@ -0,0 +1,22 @@
+package hexlet.code.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WelcomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
