cb937c1620e92f6bf313da7b75ca91cef8cb476a
zHd4 <computersystembase@gmail.com>
2024-04-11 00:45:14 +0200

    Update UserUpdateDTO

--- app/src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
+++ app/src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
@@ -1,5 +1,7 @@
 package hexlet.code.app.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.openapitools.jackson.nullable.JsonNullable;
@@ -7,6 +9,9 @@ import org.openapitools.jackson.nullable.JsonNullable;
 @Getter
 @Setter
 public class UserUpdateDTO {
+    @Email
     private JsonNullable<String> email;
+
+    @NotNull
     private JsonNullable<String> password;
 }
