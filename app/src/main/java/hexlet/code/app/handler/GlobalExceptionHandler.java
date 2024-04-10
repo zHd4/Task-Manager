ee5630636008ed8be3cad32645f4c9a1416f3b70
zHd4 <computersystembase@gmail.com>
2024-04-11 00:56:20 +0200

    Create GlobalExceptionHandler

---
+++ app/src/main/java/hexlet/code/app/handler/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package hexlet.code.app.handler;
+
+import hexlet.code.app.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
