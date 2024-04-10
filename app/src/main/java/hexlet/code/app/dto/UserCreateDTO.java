30cf76029cb519c4373fb9d0ef6a18eb4867bc30
zHd4 <computersystembase@gmail.com>
2024-04-11 00:45:06 +0200

    Update UserCreateDTO

--- app/src/main/java/hexlet/code/app/dto/UserCreateDTO.java
+++ app/src/main/java/hexlet/code/app/dto/UserCreateDTO.java
@@ -1,13 +1,23 @@
 package hexlet.code.app.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserCreateDTO {
+    @Email
     private String email;
+
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @NotNull
     private String password;
 }
