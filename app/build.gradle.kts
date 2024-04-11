d5c653ea2e9a0c64e93452ec4c1d175a0a7b46ce
zHd4 <computersystembase@gmail.com>
2024-04-11 21:44:40 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
+	implementation("net.datafaker:datafaker:2.0.2")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
 	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
