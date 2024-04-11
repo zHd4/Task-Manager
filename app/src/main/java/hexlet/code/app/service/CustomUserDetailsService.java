0c77a3c970892f625d82b7672873c363f719e9de
zHd4 <computersystembase@gmail.com>
2024-04-11 21:19:40 +0200

    Fix CustomUserDetailsService

--- app/src/main/java/hexlet/code/app/service/CustomUserDetailsService.java
+++ app/src/main/java/hexlet/code/app/service/CustomUserDetailsService.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsManager {
     @Autowired
     private UserRepository userRepository;
