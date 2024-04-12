142ceaeedcd0ba982dcd38b276632fedd6130f9c
zHd4 <computersystembase@gmail.com>
2024-04-12 17:27:18 +0200

    Update UsersControllerTest

--- app/src/test/java/hexlet/code/app/controller/UsersControllerTest.java
+++ app/src/test/java/hexlet/code/app/controller/UsersControllerTest.java
@@ -1,17 +1,21 @@
 package hexlet.code.app.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.app.dto.UserUpdateDTO;
 import hexlet.code.app.mapper.UserMapper;
 import hexlet.code.app.model.User;
 import hexlet.code.app.repository.UserRepository;
 import hexlet.code.app.util.ModelGenerator;
+import net.datafaker.Faker;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -22,6 +26,7 @@ import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,11 +35,16 @@ import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UsersControllerTest {
+    private static final Faker faker = new Faker();
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -101,5 +111,30 @@ public class UsersControllerTest {
         assertThat(user.getLastName()).isEqualTo(testUser.getLastName());
         assertThat(user.getEmail()).isEqualTo(testUser.getEmail());
         assertThat(user.getPassword()).isEqualTo(testUser.getPassword());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        userRepository.save(testUser);
+        UserUpdateDTO userData = new UserUpdateDTO();
+
+        userData.setEmail(JsonNullable.of(faker.internet().emailAddress()));
+        userData.setPassword(JsonNullable.of(faker.internet().password(5, 30)));
+
+        MockHttpServletRequestBuilder request = put("/users/{id}", testUser.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userData));
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+
+        Optional<User> userOptional = userRepository.findById(testUser.getId());
+
+        assertThat(userOptional.isPresent()).isTrue();
+
+        User user = userOptional.get();
+
+        assertThat(user.getEmail()).isEqualTo(userData.getEmail().get());
+        assertThat(user.getPassword()).isEqualTo(passwordEncoder.encode(userData.getPassword().get()));
     }
 }
