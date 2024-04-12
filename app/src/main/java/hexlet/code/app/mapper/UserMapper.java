8e30bac150a8895fe61a624d89fe943fc8ed43e8
zHd4 <computersystembase@gmail.com>
2024-04-12 21:58:32 +0200

    Update UserMapper

--- app/src/main/java/hexlet/code/app/mapper/UserMapper.java
+++ app/src/main/java/hexlet/code/app/mapper/UserMapper.java
@@ -6,6 +6,7 @@ import hexlet.code.app.dto.UserUpdateDTO;
 import hexlet.code.app.model.User;
 import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
@@ -24,8 +25,21 @@ public abstract class UserMapper {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "firstName", target = "firstName")
+    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "password", target = "password")
     public abstract User map(UserCreateDTO dto);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "firstName", target = "firstName")
+    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "createdAt", target = "createdAt")
     public abstract UserDTO map(User model);
+
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "password", target = "password")
     public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
 
     @BeforeMapping
