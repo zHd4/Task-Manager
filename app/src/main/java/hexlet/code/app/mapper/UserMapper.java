6ac6e149b88a5ddd9e9335e0fba317b06a5d5fb0
zHd4 <computersystembase@gmail.com>
2024-04-14 20:12:27 +0200

    Update UserMapper

--- app/src/main/java/hexlet/code/app/mapper/UserMapper.java
+++ app/src/main/java/hexlet/code/app/mapper/UserMapper.java
@@ -4,16 +4,11 @@ import hexlet.code.app.dto.UserCreateDTO;
 import hexlet.code.app.dto.UserDTO;
 import hexlet.code.app.dto.UserUpdateDTO;
 import hexlet.code.app.model.User;
-import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
-import org.openapitools.jackson.nullable.JsonNullable;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(
         uses = { JsonNullableMapper.class, ReferenceMapper.class },
@@ -22,25 +17,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class UserMapper {
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     public abstract User map(UserCreateDTO dto);
 
     public abstract UserDTO map(User model);
 
     public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
-
-    @BeforeMapping
-    public void encryptPassword(UserCreateDTO dto) {
-        String password = dto.getPassword();
-        dto.setPassword(passwordEncoder.encode(password));
-    }
-    @BeforeMapping
-    public void encryptPassword(UserUpdateDTO dto) {
-        if (dto.getPassword().isPresent()) {
-            String password = dto.getPassword().get();
-            dto.setPassword(JsonNullable.of(passwordEncoder.encode(password)));
-        }
-    }
 }
