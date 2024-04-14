b1baa9d092935757c2b7f0650d8cd674c0869ce2
zHd4 <computersystembase@gmail.com>
2024-04-15 01:28:36 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,28 +30,24 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-configuration-processor")
-	implementation("org.postgresql:postgresql")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
+	implementation("org.postgresql:postgresql")
 	implementation("net.datafaker:datafaker:2.0.2")
 	implementation("org.instancio:instancio-junit:3.3.1")
 
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-
 	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
-	implementation("org.mapstruct:mapstruct:1.5.5.Final")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 
-	testImplementation("org.projectlombok:lombok")
+	implementation("org.mapstruct:mapstruct:1.5.3.Final")
+	testImplementation("org.projectlombok:lombok:1.18.28")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
 
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-
-	testAnnotationProcessor("org.projectlombok:lombok")
-
-	runtimeOnly("com.h2database:h2:2.2.224")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	compileOnly("org.projectlombok:lombok:1.18.30")
+	annotationProcessor("org.projectlombok:lombok:1.18.30")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+	testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
@@ -60,6 +56,9 @@ dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 
 	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+
+	runtimeOnly("com.h2database:h2:2.2.224")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<Test> {
