f036da32e40dd1f8e053b316caef5d2a1bb12d00
zHd4 <computersystembase@gmail.com>
2024-04-15 14:56:59 +0200

    Rename LoginService -> AuthenticationService

--- app/src/main/java/hexlet/code/app/service/AuthenticationService.java
+++ app/src/main/java/hexlet/code/app/service/AuthenticationService.java
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.stereotype.Service;
 
 @Service
-public class LoginService {
+public class AuthenticationService {
     @Autowired
     private JWTUtils jwtUtils;
 
