1b4229573221214423d7dc5f6419d70a3e7d46a4
zHd4 <computersystembase@gmail.com>
2024-04-16 22:17:11 +0200

    Fix TaskStatus

--- app/src/main/java/hexlet/code/app/model/TaskStatus.java
+++ app/src/main/java/hexlet/code/app/model/TaskStatus.java
@@ -3,6 +3,9 @@ package hexlet.code.app.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -17,7 +20,12 @@ import java.time.LocalDate;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
-public class TaskStatus {
+public class TaskStatus implements BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private long id;
+
     @Size(min = 1)
     @Column(nullable = false)
     private String name;
