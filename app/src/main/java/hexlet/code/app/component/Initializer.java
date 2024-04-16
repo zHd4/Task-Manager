a92bbe8d9f8f1e6dae149e90097a519447f00ec9
zHd4 <computersystembase@gmail.com>
2024-04-16 17:47:11 +0200

    Create Initializer

---
+++ app/src/main/java/hexlet/code/app/component/Initializer.java
@@ -0,0 +1,53 @@
+package hexlet.code.app.component;
+
+import hexlet.code.app.dto.UserCreateDTO;
+import hexlet.code.app.mapper.UserMapper;
+import hexlet.code.app.model.User;
+import hexlet.code.app.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Initializer implements ApplicationRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private DefaultUserProperties defaultUserProperties;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        createDefaultUser();
+    }
+
+    private void createDefaultUser() {
+        String email = defaultUserProperties.getEmail();
+
+        if (userRepository.findByEmail(email).isEmpty()) {
+            UserCreateDTO userData = new UserCreateDTO();
+
+            userData.setFirstName("");
+            userData.setLastName("");
+            userData.setEmail(email);
+
+            String password = defaultUserProperties.getPassword();
+            String passwordDigest = passwordEncoder.encode(password);
+
+            userData.setPassword(passwordDigest);
+
+            User user = userMapper.map(userData);
+            userRepository.save(user);
+        }
+    }
+}
