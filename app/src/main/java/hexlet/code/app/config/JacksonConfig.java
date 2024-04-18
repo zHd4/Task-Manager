4162fc824f734e0f71eb5c71ff27b095d1c3fc50
zHd4 <computersystembase@gmail.com>
2024-04-18 22:31:43 +0200

    Update JacksonConfig

--- app/src/main/java/hexlet/code/app/config/JacksonConfig.java
+++ app/src/main/java/hexlet/code/app/config/JacksonConfig.java
@@ -1,10 +1,13 @@
 package hexlet.code.app.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JacksonConfig {
@@ -15,6 +18,10 @@ public class JacksonConfig {
         builder.serializationInclusion(JsonInclude.Include.NON_NULL)
                 .modulesToInstall(new JsonNullableModule());
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        builder.serializers(new LocalDateSerializer(formatter));
+
         return builder;
     }
+
 }
