d1c5b3c4fb35e8def52095c8b35d3eb9b5b235f7
zHd4 <computersystembase@gmail.com>
2024-04-21 23:18:26 +0200

    Update TaskStatusUpdateDTO

--- app/src/main/java/hexlet/code/app/dto/TaskStatusUpdateDTO.java
+++ app/src/main/java/hexlet/code/app/dto/TaskStatusUpdateDTO.java
@@ -3,10 +3,11 @@ package hexlet.code.app.dto;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
 
 @Getter
 @Setter
 public class TaskStatusUpdateDTO {
     @Size(min = 1)
-    private String name;
+    private JsonNullable<String> name;
 }
