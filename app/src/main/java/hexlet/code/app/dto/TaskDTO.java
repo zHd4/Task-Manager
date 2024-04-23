8ef1259b9dd1265ee83bb026dedb3be6d5c0bde5
zHd4 <computersystembase@gmail.com>
2024-04-23 23:59:04 +0200

    Create TaskDTO

---
+++ app/src/main/java/hexlet/code/app/dto/TaskDTO.java
@@ -0,0 +1,18 @@
+package hexlet.code.app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class TaskDTO {
+    private long id;
+    private int index;
+    private LocalDate createdAt;
+    private long assigneeId;
+    private String title;
+    private String content;
+    private String status;
+}
