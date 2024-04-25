9510b42d4170947a3e5ba6cf7ac1634f09770352
zHd4 <computersystembase@gmail.com>
2024-04-25 21:29:40 +0200

    Delete ResourceConflictException.java

--- app/src/main/java/hexlet/code/app/exception/ResourceConflictException.java
+++
@@ -1,7 +0,0 @@
-package hexlet.code.app.exception;
-
-public class ResourceConflictException extends RuntimeException {
-    public ResourceConflictException(String message) {
-        super(message);
-    }
-}
