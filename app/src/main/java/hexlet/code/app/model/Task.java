8f60a9bd052554dbe7171dfbb44f0f1670685280
zHd4 <computersystembase@gmail.com>
2024-04-25 21:51:34 +0200

    Fix linter errors

--- app/src/main/java/hexlet/code/app/model/Task.java
+++ app/src/main/java/hexlet/code/app/model/Task.java
@@ -3,7 +3,6 @@ package hexlet.code.app.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
