5efd3545bde055525207e3cc9481af3e173dfe5c
zHd4 <computersystembase@gmail.com>
2024-04-25 21:29:51 +0200

    Update TaskRepository

--- app/src/main/java/hexlet/code/app/repository/TaskRepository.java
+++ app/src/main/java/hexlet/code/app/repository/TaskRepository.java
@@ -1,6 +1,8 @@
 package hexlet.code.app.repository;
 
 import hexlet.code.app.model.Task;
+import hexlet.code.app.model.TaskStatus;
+import hexlet.code.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import java.util.Optional;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Optional<Task> findByName(String name);
+    Optional<Task> findByTaskStatus(TaskStatus status);
+    Optional<Task> findByAssignee(User assignee);
 }
