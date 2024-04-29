a65e4a15dd28fad80ab7448ac9a203609f7fea95
zHd4 <computersystembase@gmail.com>
2024-04-29 21:33:00 +0200

    Update TaskCreateDTO

--- app/src/main/java/hexlet/code/app/dto/TaskCreateDTO.java
+++ app/src/main/java/hexlet/code/app/dto/TaskCreateDTO.java
@@ -1,18 +1,24 @@
 package hexlet.code.app.dto;
 
+import hexlet.code.app.model.Label;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+import java.util.Set;
 
 @Getter
 @Setter
 public class TaskCreateDTO {
     private long index;
-    private long assigneeId;
+    private JsonNullable<Long> assigneeId;
 
     @NotBlank
     private String title;
 
-    private String content;
-    private String status;
+    private JsonNullable<String> content;
+    private JsonNullable<String> status;
+
+    private JsonNullable<Set<Label>> labels;
 }
