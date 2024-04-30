c419a0258e1f0e098cbb25d65ae4662009697759
zHd4 <computersystembase@gmail.com>
2024-04-30 20:45:36 +0200

    Update TaskDTO

--- app/src/main/java/hexlet/code/app/dto/TaskDTO.java
+++ app/src/main/java/hexlet/code/app/dto/TaskDTO.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -15,4 +16,5 @@ public class TaskDTO {
     private String title;
     private String content;
     private String status;
+    private Set<Long> taskLabelIds;
 }
