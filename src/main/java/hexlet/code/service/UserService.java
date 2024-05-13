a46a73dcc91577daa8002e7bd42ac4aa3fe7c453
zHd4 <computersystembase@gmail.com>
2024-05-13 17:20:56 +0200

    Refactor UserService

--- src/main/java/hexlet/code/service/UserService.java
+++ src/main/java/hexlet/code/service/UserService.java
@@ -5,13 +5,11 @@ import hexlet.code.dto.UserCreateDTO;
 import hexlet.code.dto.UserDTO;
 import hexlet.code.dto.UserUpdateDTO;
 import hexlet.code.exception.ResourceAlreadyExistsException;
-import hexlet.code.exception.ResourceForbiddenException;
 import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.UserMapper;
 import hexlet.code.model.User;
 import hexlet.code.repository.TaskRepository;
 import hexlet.code.repository.UserRepository;
-import hexlet.code.util.UserUtils;
 import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,21 +32,7 @@ public class UserService {
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    private UserUtils userUtils;
-
-    @Autowired
     private DefaultUserProperties defaultUserProperties;
-
-    private void checkModificationAccess(User user) {
-        User currentUser = userUtils.getCurrentUser();
-        User admin = userRepository.findByEmail(defaultUserProperties.getEmail()).get();
-
-        if (currentUser == null
-                || (user.getId() != currentUser.getId()
-                && admin.getId() != currentUser.getId())) {
-            throw new ResourceForbiddenException("Access denied");
-        }
-    }
 
     public List<UserDTO> getAll() {
         return userRepository.findAll().stream()
@@ -83,9 +67,7 @@ public class UserService {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
-        checkModificationAccess(user);
         userMapper.update(userData, user);
-
         JsonNullable<String> passwordNullable = userData.getPassword();
 
         if (passwordNullable != null && passwordNullable.isPresent()) {
@@ -102,8 +84,6 @@ public class UserService {
     public void delete(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-
-        checkModificationAccess(user);
 
         if (taskRepository.findByAssignee(user).isPresent()) {
             throw new ResourceAlreadyExistsException("User related with some task");
