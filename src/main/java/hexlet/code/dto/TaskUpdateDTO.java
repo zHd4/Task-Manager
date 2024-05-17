7cc6c84422550778d242d0605b13748ecab98a6b
zHd4 <computersystembase@gmail.com>
2024-05-17 20:58:45 +0200

    Update TaskUpdateDTO

--- src/main/java/hexlet/code/dto/TaskUpdateDTO.java
+++ src/main/java/hexlet/code/dto/TaskUpdateDTO.java
@@ -1,15 +1,24 @@
 package hexlet.code.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.openapitools.jackson.nullable.JsonNullable;
 
+import java.util.Set;
+
 @Getter
 @Setter
 public class TaskUpdateDTO {
+    @JsonProperty("assignee_id")
+    private JsonNullable<Long> assigneeId;
+
     @NotBlank
     private JsonNullable<String> title;
 
+    private JsonNullable<Long> index;
     private JsonNullable<String> content;
+    private JsonNullable<String> status;
+    private JsonNullable<Set<Long>> taskLabelIds;
 }
