169af89b9b14908e17ede4d3c796b7aec7e9fef3
zHd4 <computersystembase@gmail.com>
2024-10-21 16:45:21 +0200

    Update DefaultUserProperties

--- src/main/java/com/zhd4/taskmanager/component/DefaultUserProperties.java
+++ src/main/java/com/zhd4/taskmanager/component/DefaultUserProperties.java
@@ -2,14 +2,14 @@ package com.zhd4.taskmanager.component;
 
 import lombok.Getter;
 import lombok.Setter;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-//@ConfigurationProperties(prefix = "user")
+@ConfigurationProperties(prefix = "user")
 @Getter
 @Setter
 public class DefaultUserProperties {
-    private String email = "admin@example.com";
-    private String password = "qwerty";
+    private String email;
+    private String password;
 }
